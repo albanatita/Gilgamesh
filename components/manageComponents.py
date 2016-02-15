362719de9353c1fd70225791cb817eba1697510a
albanatita <rodolphedinca@gmail.com>
2016-02-15 12:42:10 +0100

    added missing py files

---
+++ components/manageComponents.py
@@ -0,0 +1,14 @@
+import component as cpt
+import gilgamesh as gilga
+
+cpt.initComponentDB()
+ishtar=cpt.Testbed('IShTAR')
+diags=cpt.Diagnostics('Diagnostics')
+probe1=cpt.LangmuirProbe('Langmuir1','cylNC',0.0002,0.03,'Lang_U','Lang_I')
+diags.add_child(probe1)
+ishtar.add_child(diags)
+cpt.save('v1',ishtar)
+liste=gilga.listShots(criterion='index>1000&index<=1300')
+listshots=liste.index.values
+cpt.attachshot('v1',listshots)
+cpt.componentShot()
